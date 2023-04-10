ededdfc6f7e373a8e8055bb6e1947c6c4fabf348
David Klee <klee.d@northeastern.edu>
2023-04-10 10:52:10 -0400

    update

--- main.js
+++ main.js
@@ -11,6 +11,21 @@ const associations = [
 	 'logo' : 'assets/khoury_logo.png',
 	},
 ]
+const colabs = [
+	{'name' : 'Colab: Intro. to Spherical Harmonics',
+	 'link' : 'https://colab.research.google.com/gist/dmklee/a05c24e0b3f5a36dc9ab6765ce2f97aa/introtosphericalharmonics.ipynb',
+	 'logo' : 'assets/sphharm.png',
+	},
+	{'name' : 'Colab: Visualize Predictions',
+	 'link' : 'https://colab.research.google.com/github/dmklee/image2sphere/blob/main/visualize_trained_predictions.ipynb',
+	 'logo' : 'assets/google_pred.png',
+	},
+	{'name' : 'Colab: I2S Model Walkthrough',
+	 'link' : 'https://colab.research.google.com/github/dmklee/image2sphere/blob/main/model_walkthrough.ipynb',
+	 'logo' : 'assets/model_walkthrough.png',
+	},
+]
+
 const abstract_text = 'Predicting the pose of objects from a single image is an important but difficult computer vision problem. Methods that predict a single point estimate do not predict the pose of objects with symmetries well and cannot represent uncertainty.  Alternatively, some works predict a distribution over orientations in SO(3). However, training such models can be computation- and sample-inefficient. Instead, we propose a novel mapping of features from the image domain to the 3D rotation manifold. Our method then leverages SO(3) equivariant layers, which are more sample efficient, and outputs a distribution over rotations that can be sampled at arbitrary resolution. We demonstrate the effectiveness of our method at object orientation prediction, and achieve state-of-the-art performance on the popular PASCAL3D+ dataset. Moreover, we show that our method can model complex object symmetries, without any modifications to the parameters or loss function.' 
 
 function make_header(name) {
@@ -105,11 +120,44 @@ body.append('div')
 	.attr('src', 'https://www.youtube.com/embed/yZz3umtyEEk')
 
 make_header('Code')
-body.append('div')
-	.text('View the code on Github ')
-	.append('a')
+code_body = body.append('div').style('font-size', '1.0rem')
+code_body.append('span')
+	.text('The code is available ')
+code_body.append('a')
 	.attr('href', 'https://github.com/dmklee/image2sphere')
-	.text('here.')
+	.text('here')
+code_body.append('span')
+	.text(`. We provide training scripts to reproduce results, pre-trained model weights. 
+For a quick intro to the code, check out the Colab notebooks:
+`)
+
+for (let i=0; i < colabs.length; i++) {
+	colab_div = body.append('div')
+		.attr('class', 'flex-row')
+		.style('max-height', '90px')
+		.style('border', '1px solid black')
+		.style('border-radius', '10px')
+		.style('overflow', 'hidden')
+		.style('position', 'relative')
+		.on('mouseover', function(d) {
+			d3.select(this).style('border', '3px solid black')
+		})
+		.on('mouseout', function(d) {
+			d3.select(this).style('border', '1px solid black')
+		})
+	colab_div.append('a')
+		.attr('href', colabs[i].link)
+		.append('img')
+		.attr('src', colabs[i].logo)
+		.style('width', '100%')
+		.style('transform', 'translateY(-20%)')
+	colab_div.append('p').style('font-weight', 'bold')
+		  .style('position', 'absolute')
+		  .style('left', '2%')
+		  .style('top', '-5%')
+		  .text(colabs[i].name)
+}
+
 
 make_header('Citation')
 body.append('div')
@@ -136,6 +184,7 @@ body.selectAll('.flex-row')
     .style('justify-content', 'center')
     .style('flex-direction', 'row')
     .style('width', '100%')
+
 body.selectAll('a').style('color', 'blue')
 body.selectAll('.content')
 	.style('margin', '20px auto')
